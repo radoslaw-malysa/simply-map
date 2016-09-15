3bbb442a00a76cddd03b1258f50452986284a610
Radosław Małysa <radek.malysa@gmail.com>
2016-09-15 21:33:12 +0200

    html attributes commit

--- js/simplymap.js
+++ js/simplymap.js
@@ -11,8 +11,8 @@
     var pluginName = "simplyMap",
         defaults = {
             zoom: 10,
-            Lat: '51.402196',
-            Lng: '21.145046',
+            lat: '51.402196',
+            lng: '21.145046',
             title: undefined,
             icon: undefined,
             styles: [{"featureType":"water","elementType":"geometry.fill","stylers":[{"color":"#d3d3d3"}]},{"featureType":"transit","stylers":[{"color":"#808080"},{"visibility":"off"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"visibility":"on"},{"color":"#b3b3b3"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"road.local","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#ffffff"},{"weight":1.8}]},{"featureType":"road.local","elementType":"geometry.stroke","stylers":[{"color":"#d7d7d7"}]},{"featureType":"poi","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#ebebeb"}]},{"featureType":"administrative","elementType":"geometry","stylers":[{"color":"#a7a7a7"}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"landscape","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#efefef"}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#696969"}]},{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"visibility":"on"},{"color":"#737373"}]},{"featureType":"poi","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road.arterial","elementType":"geometry.stroke","stylers":[{"color":"#d6d6d6"}]},{"featureType":"road","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{},{"featureType":"poi","elementType":"geometry.fill","stylers":[{"color":"#dadada"}]}]
@@ -24,7 +24,7 @@
         //alternative config in html
         var htmlOptions = $(this.element).data();
         
-        this.settings = $.extend( {}, defaults, htmlOptions, options );
+        this.settings = $.extend( {}, defaults, options, htmlOptions);
         this._defaults = defaults;
         this._name = pluginName;
         
@@ -42,7 +42,7 @@
             var infowindow = new google.maps.InfoWindow(),
             bounds = new google.maps.LatLngBounds(),
             marker, i,
-            myLatLng = new google.maps.LatLng(this.settings.Lat, this.settings.Lng),
+            myLatLng = new google.maps.LatLng(this.settings.lat, this.settings.lng),
             title = this.settings.title;
             
             var mapOptions = {
